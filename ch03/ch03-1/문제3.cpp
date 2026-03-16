7d824f0ce9d9de1e2900ebc18aa08ee2fd589b4b
myung5eung <gunmyeong17@gmail.com>
2026-03-16 22:20:06 +0900

    Create 문제3.cpp

---
+++ ch03/ch03-1/문제3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+class Sphere {
+public:
+	int rad;
+	double getVolume();
+	double getArea();
+};
+double Sphere::getVolume()
+{
+	return  (4.0 / 3.0) * 3.14 * rad * rad * rad;
+}
+double Sphere::getArea() {
+	return 4 * 3.14 * rad * rad;
+}
+int main(void)
+{
+	Sphere c;
+	c.rad = 3;
+	cout << "구의 부피는 " << c.getVolume() << "\n";
+	cout << "구의 표면적은 " << c.getArea() << "\n";
+	
+	return 0;
+}
