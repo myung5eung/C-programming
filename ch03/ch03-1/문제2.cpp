c2bd7e767413ce477cd7e88f93dcb7fa53118d54
myung5eung <gunmyeong17@gmail.com>
2026-03-16 22:13:55 +0900

    Create 문제2.cpp

---
+++ ch03/ch03-1/문제2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+class Triangle {
+public:
+	int wid;
+	int hei;
+	double getArea();
+};
+double Triangle::getArea()
+{
+	return (wid * hei) * 0.5;
+}
+int main(void)
+{
+	Triangle t;
+	t.wid = 3;
+	t.hei = 5;
+	cout << "삼각형의 면적은" << t.getArea();
+	return 0;
+}
