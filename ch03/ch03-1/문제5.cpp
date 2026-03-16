be3f3af92d88a063efa094e2ac470019ed2662e4
myung5eung <gunmyeong17@gmail.com>
2026-03-16 22:39:05 +0900

    Create 문제5.cpp

---
+++ ch03/ch03-1/문제5.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+class Rectangle{
+public:
+	int width;
+	int height;
+	int x, y;
+	int getArea();
+	int getCircum();
+	void getCoo();
+};
+
+int Rectangle::getArea() {
+	return width * height;
+}
+
+int Rectangle::getCircum() {
+	return (width + height) * 2;
+}
+
+void Rectangle::getCoo() {
+	cout << "(" << x + width << "," << y - height << ")";
+}
+
+int main(void)
+{
+	Rectangle rect;
+	cout << "사각형의 좌측상단 좌표(x,y): ";
+	cin >> rect.x>>rect.y;
+	cout << "사각형의 폭과 높이(width,height): ";
+	cin >> rect.width >> rect.height;
+
+	cout << "사각형의 면적은 " << rect.getArea() << "\n";
+	cout << "사각형의 둘레길이는 " << rect.getCircum() << "\n";
+	cout << "사각형의 우측하단 좌표는 ";
+	rect.getCoo();
+
+	return 0;
+}
