499112f421f1426addcf9b7f635a8e605a2f491d
myung5eung <gunmyeong17@gmail.com>
2026-03-19 00:38:18 +0900

    Create 문제4.cpp

---
+++ ch03/ch03-2/문제4.cpp
@@ -0,0 +1,35 @@
+// *******************************************
+// 제목: 사각형 클래스 위임,타켓생성자 활용
+// 날짜: 26년 3월 18일
+// 작성자: 명승연
+// *******************************************
+
+#include <iostream>
+using namespace std;
+class Rectangle{
+	int width, height;
+	int x, y;
+public:
+Rectangle();
+Rectangle(int w, int h);
+Rectangle(int length);
+Rectangle(int x, int y, int width, int height);
+int getArea() { return width * height; }
+int getCircum(){ return (width + height) * 2; }
+void getCoo(){ cout << "(" << x + width << "," << y - height << ")"; }
+};
+
+Rectangle::Rectangle() : Rectangle(1, 1, 1, 1) {}
+Rectangle::Rectangle(int x, int y):Rectangle(x, y, 1, 1) {}
+Rectangle::Rectangle(int x, int y, int width, int height):x(x), y(y), width(width), height(height) {
+}
+int main() {
+	Rectangle rect1;					 // 모든멤버변수를1로초기화
+	Rectangle rect2(3, 5);				// x=3,y=5,width=height=1로초기화
+	Rectangle rect3(3, 5, 2, 4);		// x=3,y=5,width=2, height=4로초기화
+	cout << "rect1 둘레: " << rect1.getArea() << "\n";
+	cout << "rect2 둘레: " << rect2.getCircum() << "\n";
+	cout << "rect3 좌표: ";
+	rect3.getCoo();
+	return 0;
+}
