06404ae9cd860661db890b2f88da39b2f21b20c5
myung5eung <gunmyeong17@gmail.com>
2026-04-01 14:12:06 +0900

    Update 문제3.cpp

--- ch04/4-1/문제3.cpp
+++ ch04/4-1/문제3.cpp
@@ -1,1 +1,29 @@
-
+// *******************************************
+// 제목: This포인터 사용
+// 날짜: 26년 4월 1일
+// 작성자: 명승연
+// *******************************************
+#include <iostream>
+using namespace std;
+class Triangle {
+private:
+	int width, height;
+public:
+	Triangle() :width(1), height(1) { }
+	Triangle(int w) :width(w), height(1) {}
+	Triangle(int w, int h) :width(w), height(h) {}
+	~Triangle() {}
+	void setWidth(int width) { this->width = width; }
+	void setHeight(int height) { this->height = height; }
+	double getArea() { return width * height * 0.5; }
+};
+int main()
+{
+	Triangle tri1; 
+	cout << "삼각형의 면적은 "<< tri1.getArea() << endl;
+	Triangle tri2(10); 
+	cout << "삼각형의 면적은 " << tri2.getArea() << endl;
+	Triangle tri3(10, 2);
+	cout << "삼각형의 면적은  "<< tri3.getArea() << endl;
+	return 0;
+}
