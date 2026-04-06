bd8830a6c2eb7ca524f3c9ef51013a733caa1647
myung5eung <gunmyeong17@gmail.com>
2026-04-06 18:14:23 +0900

    Create 문제4.cpp

---
+++ ch04/4-2/문제4.cpp
@@ -0,0 +1,25 @@
+// *******************************************
+// 제목: 실수배열 동적할당
+// 날짜: 26년 4월 6일
+// 작성자: 명승연
+// *******************************************
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	int n;
+	cout << "입력할 실수의 개수를 입력하세요: ";
+	cin >> n;
+	double* p = new double[n];
+	double max = p[0];
+	cout <<n<< "개의 실수를 입력 하시오.\n";
+	for (int i = 0; i < n; i++)
+	{
+		cin >> p[i];
+		if (max < p[i]) max = p[i];
+	}
+	cout << "최대값은" << max << "입니다.";
+	delete[]p;
+	return 0;
+}
+
