be4dab607bab0bcc0e006718e8f37beb20872a6a
myung5eung <gunmyeong17@gmail.com>
2026-04-13 23:08:38 +0900

    Create 문제4.cpp

---
+++ ch04/4-3/문제4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Triangle {
+private:
+	int width, height;
+public:
+	Triangle() :width(1), height(1) { cout << "밑변 " << width << "높이 " << height << "인 삼각형 생성" << "\n"; }
+	Triangle(int w, int h) :width(w), height(h) { cout << "밑변 " << width << "높이 " << height << "인 삼각형 생성" << "\n"; }
+	~Triangle(){ cout << "밑변 " << width << "높이 " << height << "인 삼각형 소멸" << "\n"; }
+
+	double getArea() { return width * height * 0.5; }
+};
+int main()
+{
+	Triangle* tri = new Triangle[3]{ Triangle(), Triangle(2,2), Triangle(4,4) };
+	for (int i = 0; i < 3; i++)
+	{
+		cout << "삼각형의 면적은" << tri[i].getArea() << "\n";
+	}
+	delete[]tri;
+	return 0;
+}
