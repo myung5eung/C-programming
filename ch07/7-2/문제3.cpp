6cc9bf8733ed61a23b5f23405bc49267807a7b28
myung5eung <gunmyeong17@gmail.com>
2026-06-01 10:29:55 +0900

    Create 문제3.cpp

---
+++ ch07/7-2/문제3.cpp
@@ -0,0 +1,25 @@
+class Complex {
+	int x, y;
+public:
+	Complex(int x = 0, int y = 0);
+	Complex& operator+=(Complex op2);
+	void show();
+};
+Complex::Complex(int x, int y) { this->x = x; this->y = y; }
+Complex& Complex::operator+=(Complex op2) {
+	this->x += op2.x;
+	this->y += op2.y;
+	return *this;
+}
+void Complex::show() {
+	cout << x << "+" << y << "j" << endl;
+}
+int main(void) {
+	Complex a(3, 5), b(1, 1);
+	a.show();
+	b.show();
+	b += a;
+	a.show();
+	b.show();
+	return 0;
+}
